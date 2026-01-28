7a48e44ec47f12b47cf3ad009cbc0896d540576d
guomin <3393493084@qq.com>
2026-01-28 13:54:15 +0800

    Build for GitHub Pages with docs directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'docs', // 修改输出目录为docs
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'pinia'],
+          element: ['element-plus', '@element-plus/icons-vue'],
+        }
+      }
+    }
+  }
 })
